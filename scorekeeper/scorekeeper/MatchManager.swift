e6f073a6289211a7712505b3ca22183090040a56
Docker User <user@example.com>
2025-12-31 17:28:58 -0600

    Fixed audio recording format (44.1kHz) to resolve decode error 🎤🛠️

--- scorekeeper/scorekeeper/MatchManager.swift
+++ scorekeeper/scorekeeper/MatchManager.swift
@@ -206,7 +206,7 @@ class MatchManager: NSObject, ObservableObject, AVAudioRecorderDelegate {
         let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
+            AVSampleRateKey: 44100,
             AVNumberOfChannelsKey: 1,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
